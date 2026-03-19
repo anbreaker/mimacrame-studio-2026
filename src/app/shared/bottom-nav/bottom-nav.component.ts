21b9794df43464c009769d7f415a082673a40e08
anbreaker <antunez19@gmail.com>
2026-03-19 20:47:34 +0100

    style(ui): update collection icon and translate bottom nav labels

--- src/app/shared/bottom-nav/bottom-nav.component.ts
+++ src/app/shared/bottom-nav/bottom-nav.component.ts
@@ -1,12 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslocoDirective } from '@jsverse/transloco';
 
 import { ROUTES } from '@core/const/routes';
 import { CartStore } from '@core/store/cart.store';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, TranslocoDirective],
   selector: 'app-bottom-nav',
   standalone: true,
   styleUrl: './bottom-nav.component.scss',
